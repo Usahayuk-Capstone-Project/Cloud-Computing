dbc75f961d6445c7d6301291403a7f320dae7800
Febby Ananda <febby.ananda32@gmail.com>
2023-06-09 17:00:07 +0700

    New Update

--- controllers/recommenderController.js
+++ controllers/recommenderController.js
@@ -7,13 +7,11 @@ const firestore = firebase.firestore();
 const AddRecommender = async (req, res, next) => {
   const uid = req.params.uid;
   try {
-    const {jenis_usaha, skala_usaha, modal_usaha, 
+    const {skala_usaha, modal_usaha, 
       bidang_usaha, omset_usaha, usia_targetpelanggan, 
       gender_targetpelanggan, perkerjaan_targetpelanggan,
       status_targetpelanggan, jenis_lokasi} = req.body;
     await firestore.collection('users').doc(uid).set({
-      uid: uid,
-      jenis_usaha : jenis_usaha,
       skala_usaha : skala_usaha,
       modal_usaha : modal_usaha,
       bidang_usaha : bidang_usaha,
@@ -46,21 +44,7 @@ const GetRecommender = async (req, res, next) => {
   }
 };
 
-//For Update Recommender in Firestore (Database)
-const UpdateRecommender = async (req, res, next) => {
-  const uid = req.params.uid;
-  try {
-    const data = req.body;
-    const userDocRef = await firestore.collection('users').doc(uid);
-    await userDocRef.update(data);
-    res.status(200).send({ message: 'User record updated successfully' });
-  } catch (error) {
-    res.status(400).send(error.message);
-  }
-}
-
 module.exports = {
   AddRecommender,
-  GetRecommender,
-  UpdateRecommender
+  GetRecommender
 };
--- routes/recommender-routes.js
+++ routes/recommender-routes.js
@@ -1,15 +1,13 @@
 const express= require('express');
 const { 
     AddRecommender,
-    GetRecommender,
-    UpdateRecommender
+    GetRecommender
 } = require('../controllers/recommenderController');
 
 const router = express.Router();
 
 router.post('/:uid',AddRecommender);
 router.get('/:uid',GetRecommender);
-router.put('/:uid',UpdateRecommender);
 
 
 module.exports = {
